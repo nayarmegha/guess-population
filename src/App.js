7d91250b7e1a1c20e4e1bd8bc75370267535d1aa
nayar megha <me@meghanayar.com>
2023-09-02 16:40:43 -0500

    copied to new repo in personal github

--- src/App.js
+++ src/App.js
@@ -1,81 +1,42 @@
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import CharacterCard from "./CharacterCard";
-import characters from "./protagonists.json";
-import { useState } from "react";
+import Population from "./Population";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#252b36",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+    primary: {
+      main: "#2b56d6",
+      dark: "#252b36",
+    },
+    secondary: {
+      main: "#abe444",
+    },
+  },
+  typography: {
+    fontFamily: "Raleway, Arial",
+    fontSize: 28,
+  },
+});
 function App() {
-  // state variable to store the click count!
-  const [count, setCounter] = useState(0);
-
   return (
-    <div className="App">
-      <CssBaseline />
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: "1px solid lightgray" }}
-      >
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Characters Inc
-          </Typography>
-          <Button
-            href="#"
-            variant="outlined"
-            sx={{ my: 1, mx: 1.5 }}
-            onClick={() => {
-              setCounter(count + 1);
-            }}
-          >
-            COUNT BUTTON
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="md" sx={{ my: 4 }}>
-        <Typography
-          variant="h2"
-          align="center"
-          color="text.primary"
-          sx={{ py: 2 }}
-        >
-          Prevalent Protagonists
-        </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          sx={{ mx: 10 }}
-        >
-          Counter: {count}
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="lg">
-        <Grid
-          container
-          spacing={5}
-          justifyContent="center"
-          alignItems="flex-start"
-        >
-          {characters.map((character) => (
-            <Grid item xs={12} md={4} key={character.title}>
-              <CharacterCard
-                characterName={character.title}
-                image={character.pic}
-                descriptionArray={character.description}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <CssBaseline />
+        <Container maxWidth="md" justifyContent="center" sx={{ pt: 15 }}>
+          <p>
+            <b> 🤓🌍🤓 ⁉️ Which city is bigger by population ⁉️ 🤓🌍🤓</b>
+          </p>
+          <Population />
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Population.js
@@ -0,0 +1,300 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import cities from "./GenerateCountries.json";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#252b36",
+    },
+    primary: {
+      main: "#2b56d6",
+      dark: "#252b36",
+    },
+    secondary: {
+      main: "#abe444",
+    },
+  },
+  typography: {
+    fontFamily: "Raleway, Arial",
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          color: "white",
+        },
+      },
+    },
+  },
+});
+
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/vnd.teleport.v1+json");
+
+var requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+export default function Population() {
+  const [cityA, setCityA] = useState({
+    name: "",
+    pop: 0,
+  });
+  const [cityB, setCityB] = useState({
+    name: "",
+    pop: 0,
+  });
+  const [searchA, setSearchA] = useState("");
+  const [searchB, setSearchB] = useState("");
+  const [points, setPoints] = useState(0);
+
+  function getPopulation(whichCity) {
+    resetText();
+    var city = "";
+    if (whichCity == "a") {
+      //city a
+      city = document.getElementById("city-a-text").value;
+    } else {
+      //city b
+      city = document.getElementById("city-b-text").value;
+    }
+
+    fetch("https://api.teleport.org/api/cities/?search=" + city, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        const cityUrl =
+          result._embedded["city:search-results"][0]._links["city:item"].href;
+        fetch(cityUrl, requestOptions)
+          .then((response) => response.json())
+          .then((cityInfo) => {
+            if (whichCity == "a") {
+              setCityA({
+                name: cityInfo.name,
+                pop: cityInfo.population,
+              });
+            } else {
+              setCityB({
+                name: cityInfo.name,
+                pop: cityInfo.population,
+              });
+            }
+          })
+          .catch((error) => console.log("error", error));
+      })
+      .catch((error) => console.log("error", error));
+  }
+
+  function isBigger(whichCity) {
+    resetText();
+    if (whichCity == "a") {
+      if (cityA.pop >= cityB.pop) {
+        document.getElementById("correct").hidden = false;
+        document.getElementById("a-bigger").hidden = false;
+        setPoints(points + 1);
+        console.log(points);
+      } else {
+        document.getElementById("wrong").hidden = false;
+        document.getElementById("b-bigger").hidden = false;
+        setPoints(points - 1);
+        console.log(points);
+      }
+    } else {
+      // city b
+      if (cityA.pop < cityB.pop) {
+        document.getElementById("correct").hidden = false;
+        document.getElementById("b-bigger").hidden = false;
+        setPoints(points + 1);
+        console.log(points);
+      } else {
+        document.getElementById("wrong").hidden = false;
+        document.getElementById("a-bigger").hidden = false;
+        setPoints(points - 1);
+        console.log(points);
+      }
+    }
+    document.getElementById("total-score").hidden = false;
+  }
+
+  function resetText() {
+    document.getElementById("correct").hidden = true;
+    document.getElementById("a-bigger").hidden = true;
+    document.getElementById("wrong").hidden = true;
+    document.getElementById("b-bigger").hidden = true;
+  }
+
+  function resetScore() {
+    document.getElementById("total-score").hidden = true;
+  }
+
+  function generateAnyCity(whichCity) {
+    resetText();
+    var city = "";
+    var randomIndex = 0;
+    randomIndex = Math.floor(Math.random() * 10);
+    if (whichCity == "a") {
+      setCityA({
+        name: cities[randomIndex].name,
+        pop: cities[randomIndex].population,
+      });
+    } else {
+      setCityB({
+        name: cities[randomIndex].name,
+        pop: cities[randomIndex].population,
+      });
+    }
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <br></br>
+        <br></br>
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <TextField
+            id="city-a-text"
+            label="search for a city"
+            size="small"
+            sx={{ color: "#252b36" }}
+            InputLabelProps={{
+              sx: {
+                color: "white",
+              },
+            }}
+            inputProps={{
+              sx: {
+                color: "white",
+                paddingLeft: "15px",
+              },
+            }}
+            value={searchA}
+            onChange={(event) => {
+              const newValue = event.target.value;
+              setSearchA(newValue);
+            }}
+          />
+          <Button
+            id="city-a-button"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              getPopulation("a");
+            }}
+          >
+            Find City A
+          </Button>
+
+          <TextField
+            id="city-b-text"
+            label="search for a city"
+            size="small"
+            sx={{}}
+            InputLabelProps={{
+              sx: {
+                color: "white",
+              },
+            }}
+            inputProps={{
+              sx: {
+                color: "white",
+                paddingLeft: "15px",
+              },
+            }}
+            value={searchB}
+            onChange={(event) => {
+              const newValue = event.target.value;
+              setSearchB(newValue);
+            }}
+          />
+          <Button
+            id="city-b-button"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              getPopulation("b");
+            }}
+          >
+            Find City B
+          </Button>
+        </Grid>
+        <br></br>
+        {/* user wants random cities */}
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <Button
+            id="generate-random-city-a"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              generateAnyCity("a");
+            }}
+          >
+            GENERATE RANDOM CITY A
+          </Button>
+          <Button
+            id="generate-random-city-b"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              generateAnyCity("b");
+            }}
+          >
+            GENERATE RANDOM CITY B
+          </Button>
+        </Grid>
+        <br></br>
+        <br></br>
+        <b>Choose the city that is bigger by population:</b>
+        <br></br>
+        <br></br>
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <Button
+            id="city-a-bigger"
+            color="secondary"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              isBigger("a");
+            }}
+          >
+            {cityA.name} is bigger
+          </Button>
+          <Button
+            id="city-b-bigger"
+            color="secondary"
+            variant="contained"
+            sx={{ px: 6, mx: "auto" }}
+            onClick={() => {
+              isBigger("b");
+            }}
+          >
+            {cityB.name} is bigger
+          </Button>
+        </Grid>
+        <br></br>
+        <p hidden id="correct">
+          🤠✅🤠✅🤠✅🤠✅
+        </p>
+        <p hidden id="wrong">
+          😤❌😤❌😤❌😤❌
+        </p>
+        <p hidden id="a-bigger">
+          {cityA.name} (population {cityA.pop}) is bigger than {cityB.name}{" "}
+          (population {cityB.pop})
+        </p>
+        <p hidden id="b-bigger">
+          {cityB.name} (population {cityB.pop}) is bigger than {cityA.name}{" "}
+          (population {cityA.pop})
+        </p>
+        <p hidden id="total-score">
+          Score: {points}
+        </p>
+      </ThemeProvider>
+    </>
+  );
+}
